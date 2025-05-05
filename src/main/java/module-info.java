b71cebedb853569a18f7138cf1e09d1a528f7c1a
Santiago <105152807+Sangarru11@users.noreply.github.com>
2025-05-05 20:13:45 +0200

    Comienzo del proyecto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,14 @@
 module com.github.Sangarru11 {
+    requires java.naming;
     requires javafx.controls;
     requires javafx.fxml;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires java.desktop;
+    requires jdk.compiler;
 
     opens com.github.Sangarru11 to javafx.fxml;
+    opens com.github.Sangarru11.Model.Entity to org.hibernate.orm.core;
     exports com.github.Sangarru11;
+    exports com.github.Sangarru11.View;
 }
